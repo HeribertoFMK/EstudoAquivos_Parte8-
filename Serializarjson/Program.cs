ea4bf4b7ca215c140e3f06f9ff8a6fed642d7c41
Heriberto F M Komatu <hfmk2015@gmail.com>
2021-08-17 15:13:09 +0900

    Termino do estudo sobre serialização e deserialização. tive que fazer no visual studio por não conseguir fazer no visual code

--- Serializarjson/Program.cs
+++ Serializarjson/Program.cs
@@ -15,7 +15,7 @@ namespace Serializarjson
         {
 
 
-           /*
+           
                 Usuario usuario = new Usuario()
                 {
                     Nome = "JOSE SANTANA MACHADO",
@@ -23,13 +23,23 @@ namespace Serializarjson
                     email = "hfmk2015@gmail.com"
 
                 };
+            
                 StreamWriter stream = new StreamWriter(@"C:\CursoC#\outro.json");
                 JavaScriptSerializer serializador = new JavaScriptSerializer();
                 string jsonseializador = serializador.Serialize(usuario);
                 stream.WriteLine(jsonseializador);
                 stream.Close();
 
-            */
+            
+            StreamReader streamreader = new StreamReader(@"C:\CursoC#\outro.json");
+            string linhaslidas = streamreader.ReadToEnd();
+
+            JavaScriptSerializer deserializador = new JavaScriptSerializer();
+            Usuario leitura = (Usuario)deserializador.Deserialize(linhaslidas,typeof(Usuario));
+            Console.WriteLine("Nome = {0},Cpf = {1},Email = {2}",usuario.Nome,usuario.Cpf,usuario.email);
+
+            Console.ReadKey();
+
 
 
 
