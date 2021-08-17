1a86044f51963c8be6e1806e287b0a38b5d4e109
Heriberto F M Komatu <hfmk2015@gmail.com>
2021-08-17 14:38:00 +0900

    iniciando o curso de serialização e desserialização

---
+++ Serializarjson/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.IO;
+
+
+namespace Serializarjson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+           /*
+                Usuario usuario = new Usuario()
+                {
+                    Nome = "JOSE SANTANA MACHADO",
+                    Cpf = "032.323.537.97",
+                    email = "hfmk2015@gmail.com"
+
+                };
+                StreamWriter stream = new StreamWriter(@"C:\CursoC#\outro.json");
+                JavaScriptSerializer serializador = new JavaScriptSerializer();
+                string jsonseializador = serializador.Serialize(usuario);
+                stream.WriteLine(jsonseializador);
+                stream.Close();
+
+            */
+
+
+
+
+                
+
+
+            
+                
+                    
+
+
+        }
+    }
+}
---
+++ Serializarjson/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serializarjson
+{
+    class Usuario
+    {
+
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string email { get; set;}
+
+    }
+}
